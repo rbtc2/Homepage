f0c4bb64dd0e87069d9b647adfce175416a10ce3
rbtc2 <rbtc22@gmail.com>
2026-04-05 11:05:49 +0900

    홈화면/슬라이드

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,9 @@ import Header from '@/components/Header';
 import HeroBanner from '@/components/HeroBanner';
 import NoticeStrip from '@/components/NoticeStrip';
 import Footer from '@/components/Footer';
+import HomeWrNewsSpotlight from '@/components/home/HomeWrNewsSpotlight';
+
+export const revalidate = 60;
 
 export default function Home() {
   return (
@@ -11,7 +14,7 @@ export default function Home() {
       <NoticeStrip />
       <main className="main" role="main">
         <div className="main__surface">
-          <p className="main__lead">진행 예정</p>
+          <HomeWrNewsSpotlight />
         </div>
       </main>
       <Footer />
